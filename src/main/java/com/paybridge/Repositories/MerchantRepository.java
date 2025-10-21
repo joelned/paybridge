5112fdea53126291f37a6e48319bf39d2ff48149
Joel <joelekwegh3@gmail.com>
2025-10-22 00:13:17 +0100

    refractor(class): Added JPA queries to locate api test key and live key

--- src/main/java/com/paybridge/Repositories/MerchantRepository.java
+++ src/main/java/com/paybridge/Repositories/MerchantRepository.java
@@ -3,8 +3,13 @@ package com.paybridge.Repositories;
 import com.paybridge.Models.Entities.Merchant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MerchantRepository extends JpaRepository<Merchant, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<String> findByApiKeyLive(String apiKey);
+    Optional<String> findByApiKeyTest(String apiKey);
 }
 
