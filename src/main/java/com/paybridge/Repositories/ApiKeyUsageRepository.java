09681a08d4a3eb8938f9d389784789a0544f6ce4
Joel <joelekwegh3@gmail.com>
2025-10-22 10:07:15 +0100

    feat(repo): Added countByMerchantSince query

--- src/main/java/com/paybridge/Repositories/ApiKeyUsageRepository.java
+++ src/main/java/com/paybridge/Repositories/ApiKeyUsageRepository.java
@@ -1,7 +1,14 @@
 package com.paybridge.Repositories;
 
 import com.paybridge.Models.Entities.ApiKeyUsage;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface ApiKeyUsageRepository extends JpaRepository<ApiKeyUsage, Integer> {
+
+    @Query("SELECT COUNT(a) FROM ApiKeyUsage a WHERE a.merchant.id = :merchantId AND a.createdAt >= :since")
+    long countByMerchantSince(@Param("merchantId") Long merchantId, @Param("since")LocalDateTime since);
 }
