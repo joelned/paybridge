86207092a7c8ba9318c2a3f4252119f1fb1bfb42
Joel <joelekwegh3@gmail.com>
2025-10-25 02:27:00 +0100

    refractor(DTO): Added validation constraints on email and password

--- src/main/java/com/paybridge/Models/DTOs/LoginRequest.java
+++ src/main/java/com/paybridge/Models/DTOs/LoginRequest.java
@@ -1,9 +1,16 @@
 package com.paybridge.Models.DTOs;
 
-public class LoginRequest {
-    private String email;
-    private String password;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
 
     public LoginRequest(String email, String password) {
         this.email = email;
