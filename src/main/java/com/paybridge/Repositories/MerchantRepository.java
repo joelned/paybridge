efc044300f8892d70016b7050f07764290cfbc9a
Joel <joelekwegh3@gmail.com>
2025-10-26 16:11:05 +0100

    chore: Added new query to verify is merchant is enabled

--- src/main/java/com/paybridge/Repositories/MerchantRepository.java
+++ src/main/java/com/paybridge/Repositories/MerchantRepository.java
@@ -15,5 +15,11 @@ public interface MerchantRepository extends JpaRepository<Merchant, Long> {
 
     @Query("SELECT m FROM Merchant m WHERE m.apiKeyTest = :apiKey OR m.apiKeyLive = :apiKey")
     Optional<Merchant> findByApiKeyTestOrApiKeyLive(@Param("apiKey") String apiKey);
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END " +
+            "FROM Users u WHERE u.merchant.id = :merchantId " +
+            "AND u.userType = com.paybridge.Models.Enums.UserType.MERCHANT " +
+            "AND u.enabled = true")
+    boolean hasMerchantEnabledUser(@Param("merchantId") Long merchantId);
 }
 
