e95ba3fe34af9192540eb956858eb81a36611144
Joel <joelekwegh3@gmail.com>
2025-11-01 09:45:38 +0100

    chore: Improved password validation constraints

--- src/main/java/com/paybridge/Models/DTOs/MerchantRegistrationRequest.java
+++ src/main/java/com/paybridge/Models/DTOs/MerchantRegistrationRequest.java
@@ -15,10 +15,9 @@ public class MerchantRegistrationRequest {
     @Email(message = "Invalid email format")
     private String email;
 
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$",
-            message = "Password must contain at least one uppercase letter, one lowercase letter, and one number")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,64}$",
+            message = "Password must contain uppercase, lowercase, number, special character, and be 8-64 characters")
+    @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
     private String password;
 
     @NotBlank(message = "Business type is required")
