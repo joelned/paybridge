d44e7c4c687e919698262e659f3ff954060103f5
Joel <joelekwegh3@gmail.com>
2025-11-02 12:32:23 +0100

    chore: Changed type to Optional

--- src/main/java/com/paybridge/Repositories/ProviderRepository.java
+++ src/main/java/com/paybridge/Repositories/ProviderRepository.java
@@ -3,6 +3,8 @@ package com.paybridge.Repositories;
 import com.paybridge.Models.Entities.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProviderRepository extends JpaRepository<Provider, Long> {
-    Provider findByName(String name);
+    Optional<Provider> findByName(String name);
 }
