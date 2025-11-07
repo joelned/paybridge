66aa9f27d009ec4de0a573b95b49b6367a7de2db
Joel <joelekwegh3@gmail.com>
2025-11-07 10:47:14 +0100

    chore: Made function cacheable and added function to find api key by hash

--- src/main/java/com/paybridge/Repositories/MerchantRepository.java
+++ src/main/java/com/paybridge/Repositories/MerchantRepository.java
@@ -1,9 +1,10 @@
 package com.paybridge.Repositories;
 
 import com.paybridge.Models.Entities.Merchant;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -16,10 +17,14 @@ public interface MerchantRepository extends JpaRepository<Merchant, Long> {
     @Query("SELECT m FROM Merchant m WHERE m.apiKeyTest = :apiKey OR m.apiKeyLive = :apiKey")
     Optional<Merchant> findByApiKeyTestOrApiKeyLive(@Param("apiKey") String apiKey);
 
+    @Query("SELECT m FROM Merchant m WHERE m.apiKeyTestHash = :apiKeyHash OR m.apiKeyLiveHash = :apiKeyHash")
+    Optional<Merchant> findByApiKeyHash(@Param("apiKeyHash") String apiKeyHash);
+
     @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END " +
             "FROM Users u WHERE u.merchant.id = :merchantId " +
             "AND u.userType = com.paybridge.Models.Enums.UserType.MERCHANT " +
             "AND u.enabled = true")
+    @Cacheable(cacheNames = "merchantEnabledUser", key = "#merchantId")
     boolean hasMerchantEnabledUser(@Param("merchantId") Long merchantId);
 }
 
