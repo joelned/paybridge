d14e0667e95528acac7b49469909f14ebb879c83
Joel <joelekwegh3@gmail.com>
2025-11-14 13:45:39 +0100

    chore: Added @EnableAsync

--- src/main/java/com/paybridge/Configs/AsyncConfig.java
+++ src/main/java/com/paybridge/Configs/AsyncConfig.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskDecorator;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig {
     @Bean
     public TaskExecutor taskExecutor() {
