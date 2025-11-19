070fc5110757b1b140e584662fb422f4da228205
Joel <joelekwegh3@gmail.com>
2025-11-19 21:51:39 +0100

    chore: Added findByIdempotencyKey query

--- src/main/java/com/paybridge/Repositories/IdempotencyKeyRepository.java
+++ src/main/java/com/paybridge/Repositories/IdempotencyKeyRepository.java
@@ -3,5 +3,8 @@ package com.paybridge.Repositories;
 import com.paybridge.Models.Entities.IdempotencyKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IdempotencyKeyRepository extends JpaRepository<IdempotencyKey, Integer> {
+    Optional<IdempotencyKey> findByIdempotencyKey(String idempotencyKey);
 }
