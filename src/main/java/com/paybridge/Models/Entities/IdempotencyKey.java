b65b47f58e40c46621b8261218158cf42a9e42a6
Joel <joelekwegh3@gmail.com>
2025-11-19 21:52:08 +0100

    feat(entity): Added new columns

--- src/main/java/com/paybridge/Models/Entities/IdempotencyKey.java
+++ src/main/java/com/paybridge/Models/Entities/IdempotencyKey.java
@@ -11,6 +11,8 @@ public class IdempotencyKey {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name="idempotency_key", nullable = false)
+    private String idempotencyKey;
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
@@ -24,5 +26,65 @@ public class IdempotencyKey {
     @CreationTimestamp
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+    @CreationTimestamp
+    @Column(name = "expires_at")
+    private LocalDateTime expiresAt;
+    @Column(name = "locked")
+    private boolean locked = false;
 
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getRequestHash() {
+        return requestHash;
+    }
+
+    public void setRequestHash(String requestHash) {
+        this.requestHash = requestHash;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public PaymentStatus getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(PaymentStatus paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
 }
