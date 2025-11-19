be4b016225e2391b002a370a88988e851349aca4
Joel <joelekwegh3@gmail.com>
2025-11-19 21:53:26 +0100

    chore: Added getters and setters because for some reason lombok fails in production

--- src/main/java/com/paybridge/Models/DTOs/CreatePaymentRequest.java
+++ src/main/java/com/paybridge/Models/DTOs/CreatePaymentRequest.java
@@ -14,5 +14,47 @@ public class CreatePaymentRequest {
     @NotNull @Size(min = 3, max = 3) private String currency;
     @Email private String email;
     @NotBlank private String description;
-    private Map<String, String> metadata;
+    @NotBlank private Map<String, String> metadata;
+
+
+    public Map<String, String> getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Map<String, String> metadata) {
+        this.metadata = metadata;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
 }
